ed30abbeec43cf562f682f3f9ef00f26a3456389
Mikko Suniala <mikko.suniala@iki.fi>
2023-07-05 17:16:46 +0300

    Add Ktor, logging

--- src/main/kotlin/kotlinbook/Main.kt
+++ src/main/kotlin/kotlinbook/Main.kt
@@ -1,5 +1,25 @@
 package kotlinbook
 
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.slf4j.LoggerFactory
+
+private val log = LoggerFactory.getLogger("kotlinbook.Main")
+
 fun main() {
-    println("Hello, World!")
+    log.debug("Starting application...")
+
+    embeddedServer(Netty, port = 4207) {
+        createKtorApplication()
+    }.start(wait = true)
+}
+fun Application.createKtorApplication() {
+    routing {
+        get("/") {
+            call.respondText("Hello, World!")
+        }
+    }
 }
