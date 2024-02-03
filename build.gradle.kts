313167cf562e7f73b3d94ed0f8ea3f143dbda4eb
Mikko Suniala <mikko.suniala@iki.fi>
2024-02-03 17:25:40 +0200

    build: upgrade all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,33 +13,33 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:2.1.2")
-    implementation("io.ktor:ktor-server-netty:2.1.2")
+    implementation("io.ktor:ktor-server-core:2.2.4")
+    implementation("io.ktor:ktor-server-netty:2.2.4")
 
-    implementation("io.ktor:ktor-client-core:2.1.2")
+    implementation("io.ktor:ktor-client-core:2.2.4")
     // CIO (coroutine I/O) based backend for ktor-client
-    implementation("io.ktor:ktor-client-cio:2.1.2")
+    implementation("io.ktor:ktor-client-cio:2.2.4")
 
-    implementation("io.ktor:ktor-server-auth:2.1.2")
-    implementation("io.ktor:ktor-server-sessions:2.1.2")
+    implementation("io.ktor:ktor-server-auth:2.2.4")
+    implementation("io.ktor:ktor-server-sessions:2.2.4")
 
-    implementation("io.arrow-kt:arrow-fx-coroutines:1.1.2")
-    implementation("io.arrow-kt:arrow-fx-stm:1.1.2")
+    implementation("io.arrow-kt:arrow-fx-coroutines:1.1.5")
+    implementation("io.arrow-kt:arrow-fx-stm:1.1.5")
 
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("com.h2database:h2:2.1.214")
-    implementation("org.flywaydb:flyway-core:9.5.1")
+    implementation("org.flywaydb:flyway-core:9.16.0")
     implementation("com.github.seratch:kotliquery:1.9.0")
 
-    implementation("io.ktor:ktor-server-html-builder:2.1.2")
+    implementation("io.ktor:ktor-server-html-builder:2.2.4")
 
-    implementation("at.favre.lib:bcrypt:0.9.0")
+    implementation("at.favre.lib:bcrypt:0.10.2")
 
-    implementation("ch.qos.logback:logback-classic:1.4.4")
-    implementation("org.slf4j:slf4j-api:2.0.3")
+    implementation("ch.qos.logback:logback-classic:1.4.7")
+    implementation("org.slf4j:slf4j-api:2.0.5")
 
     implementation("com.typesafe:config:1.4.2")
-    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     testImplementation(kotlin("test"))
 }
